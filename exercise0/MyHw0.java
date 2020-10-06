1e3f50d00720805d8ee406b7300c65c011f76623
petsinis <32618554+petsinis@users.noreply.github.com>
2020-10-06 19:56:22 +0300

    Add files via upload

--- exercise0/MyHw0.java
+++ exercise0/MyHw0.java
@@ -1,26 +1,26 @@
 import java.util.HashMap; 
 import java.util.LinkedList; 
 import java.util.Queue;
-import  javafx.util.Pair;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class MyHw0 {
  public static void main(String[] args) throws FileNotFoundException, IOException {
-   Queue<Pair> id=new LinkedList<>();
-   HashMap<Integer, Integer> mapk=new HashMap<Integer, Integer>();
-   HashMap<Integer, HashMap<Integer, Integer>> map=new HashMap<Integer, HashMap<Integer, Integer>>();
+   Queue<LinkedList<Integer>> id=new LinkedList<>();
+   HashMap<Integer, Integer> mapk=new HashMap<>();
+   HashMap<Integer, HashMap<Integer, Integer>> map=new HashMap<>();
    try (BufferedReader br=new BufferedReader(new InputStreamReader(System.in))) {
     String l;
     while ((l=br.readLine())!=null){
       String t[]=l.split("\\|");
       int k=Integer.parseInt(t[0]);
       int v=Integer.parseInt(t[1]);
-      Pair<Integer, Integer> mp;
-      mp=new Pair<>(k,v);
+      LinkedList<Integer> mp=new LinkedList<>();
+      mp.add(k);
+      mp.add(v);
       id.add(mp);
-      HashMap<Integer, Integer> temp=new HashMap<Integer, Integer>();         
+      HashMap<Integer, Integer> temp=new HashMap<>();         
       if(mapk.containsKey(k)){
         temp=map.get(k);
         if(temp.containsKey(v)){
@@ -40,10 +40,10 @@ public class MyHw0 {
    }
   }
   while(!(id.isEmpty())){
-    Pair mp=id.remove();
-    int k=(int)mp.getKey();
-    int v=(int)mp.getValue();
-    System.out.println(mp.getKey()+"|"+mp.getValue()+"["+map.get(k).get(v)+" of "+mapk.get(k)+"]");
+    LinkedList mp=id.remove();
+    int k=(int) mp.remove();
+    int v=(int) mp.remove();
+    System.out.println(""+k+"|"+v+"["+map.get(k).get(v)+" of "+mapk.get(k)+"]");
   }
  }
 }
